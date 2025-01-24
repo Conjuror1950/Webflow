45f17731a17c0ed62d830be6cf5e6f1c0d74f12b
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-01-24 13:08:22 +0100

    Update Remove-Webflow-Badge.js

--- Remove-Webflow-Badge.js
+++ Remove-Webflow-Badge.js
@@ -1,6 +1,11 @@
-<style>
-  .w-webflow-badge {
-   display: None !important;
-   visibility: hidden !important;
-}
-  </style>
+(function() {
+    var style = document.createElement("style");
+    style.innerHTML = `
+        .w-webflow-badge {
+            display: none !important;
+            visibility: hidden !important;
+        }
+    `;
+    document.head.appendChild(style);
+    console.log("Badge Webflow rimosso!");
+})();
