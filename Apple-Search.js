7fb3897a4225cd514ff230d333447dedb8ce37cb
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-02-04 21:32:22 +0100

    Apple-Search.js

--- Apple-Search.js
+++ Apple-Search.js
@@ -61,6 +61,7 @@ const appleSearchCSS = `
 
 .apple-search-box {
     max-width: 600px;
+    margin-left: 410px;
     display: flex;
     align-items: center;
     border-bottom: 1px solid rgba(255, 255, 255, 0.4);
