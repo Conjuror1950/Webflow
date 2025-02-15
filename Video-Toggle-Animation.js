a611b807048a75251bc3240385ed70206d21deb6
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-02-15 11:17:03 +0100

    Video-Toggle-Animation.js

--- Video-Toggle-Animation.js
+++ Video-Toggle-Animation.js
@@ -54,7 +54,11 @@
       }
 
       // Associa l'evento click all'icona per gestire play/pause
-      $("#video-toggle, #mute, #full-screen").click(togglePlayPause);
+      $("#video-toggle").click(togglePlayPause); // Solo questa icona gestisce play/pause
+$("#mute").click(function (e) {
+  e.stopPropagation(); // Evita qualsiasi azione per questa icona
+});
+
 
       // Dopo 10 secondi mostra l'icona con un effetto di dissolvenza
       setTimeout(function () {
