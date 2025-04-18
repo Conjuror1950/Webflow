c6aaf0b2273b6d6057be35df7ea8a9bda3168bc5
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-04-18 15:07:54 +0200

    Gallery-Volume2-paesaggi-naturali-mobile.js

--- Gallery-Volume2-paesaggi-naturali-mobile.js
+++ Gallery-Volume2-paesaggi-naturali-mobile.js
@@ -71,8 +71,8 @@
       align-items: center;
     }
     .product-image-Volume2-paesaggi-naturali-mobile {
-    width: 100%;
-    height: 65vh;
+    max-width: 100%;
+    max-height: 70vh;
     object-fit: cover; /* oppure "contain" se preferisci vedere l’immagine intera */
     border-radius: 2%;
     cursor: pointer;
