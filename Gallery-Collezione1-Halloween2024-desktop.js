801747e94e9ca254ba94c80ef8eeb12f02b928fa
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-04-24 14:00:56 +0200

    Gallery-Collezione1-Halloween2024-desktop.js

--- Gallery-Collezione1-Halloween2024-desktop.js
+++ Gallery-Collezione1-Halloween2024-desktop.js
@@ -362,12 +362,6 @@
   width: 40px;
   height: 40px;
 }
-/* override specifico per full-screen: resetto il translateX e sposto manualmente 2px */
-:fullscreen .slider-button-Collezione1-halloween2024-desktop.next,
-:-webkit-full-screen .slider-button-Collezione1-halloween2024-desktop.next {
-  transform: translateY(-50%) !important;   /* rimuove il translateX precedente */
-  right: 18px !important;                    /* 20px–2px = 18px dal bordo */
-}
 
 }
     /* Media query: visualizza solo su desktop (>= 1280px) */
