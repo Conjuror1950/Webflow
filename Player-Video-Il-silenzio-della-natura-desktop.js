9a94d8ae59d032e4735df21ff84b0e1e0f426a7c
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-05-20 10:44:52 +0200

    Player-Video-Il-silenzio-della-natura-desktop.js

--- Player-Video-Il-silenzio-della-natura-desktop.js
+++ Player-Video-Il-silenzio-della-natura-desktop.js
@@ -481,7 +481,7 @@ justify-content:flex-end;
   border: none;
   cursor: pointer;
   width: 100%;               /* per far coprire l’hover a tutta larghezza */
-  padding: 0.4rem 0.4rem;      /* uguale spazio sopra e sotto, e orizzontale */
+  padding: 0.2rem 0.2rem;      /* uguale spazio sopra e sotto, e orizzontale */
 }
 
 /* separatore sotto ogni voce tranne l’ultima */
