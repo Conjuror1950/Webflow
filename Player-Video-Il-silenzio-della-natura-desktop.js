0566759d8869a292419467b454b01a0b9d864211
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-05-20 23:01:27 +0200

    Update Player-Video-Il-silenzio-della-natura-desktop.js

--- Player-Video-Il-silenzio-della-natura-desktop.js
+++ Player-Video-Il-silenzio-della-natura-desktop.js
@@ -203,8 +203,8 @@ justify-content:space-between;
 .play-pause .play-icon,
 .play-pause .pause-icon {
   position: relative;
-  top: 150px;
-  left: 120px;
+  top: 120px;
+  left: 110px;
   width: 100%;
   height: 100%;
 }
