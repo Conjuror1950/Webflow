57f9aa4c174742fbb1485cb3ce9145920d57791a
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-05-23 16:12:12 +0200

    Update Player-Video-Il-silenzio-della-natura-desktop.js

--- Player-Video-Il-silenzio-della-natura-desktop.js
+++ Player-Video-Il-silenzio-della-natura-desktop.js
@@ -1295,32 +1295,5 @@ emailBtn.addEventListener('click', () => {
    langMenu.style.display = 'none';
  });
 };
-    
-  function formatTime(s) {
-    const m=Math.floor(s/60), sec=Math.floor(s%60).toString().padStart(2,'0');
-    return `${m}:${sec}`;
-  }
-
-// apri/chiudi menu lingua
-langBtn.addEventListener('click', e => {
-  e.stopPropagation();
-  shareMenu.style.display = 'none';      // ← chiudo il menu share
-  subsMenu.style.display   = 'none';    // ← CHIUDI SEMPRE IL MENU SOTTOTITOLI
-  langMenu.style.display = langMenu.style.display === 'flex' ? 'none' : 'flex';
-});
-
-// clic su voce di lingua
-langMenu.querySelectorAll('.lang-item-player-video-il-silenzio-della-natura-desktop').forEach(item => {
-  item.addEventListener('click', () => {
-    const newLang = item.dataset.lang;            // "it" oppure "en"
-    document.documentElement.lang = newLang;      // imposta lang sull’<html>
-    // (qui potresti aggiungere logica di i18n o ricaricare la pagina)
-    langMenu.style.display = 'none';
-    // opzionale: cambiare tooltip o titolo del button
-    langBtn.title = newLang === 'it' ? 'Italiano' : 'English';
-  });
-});
-
-};
   document.body.appendChild(dashScript);
 })();
