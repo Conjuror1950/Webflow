17ccce9734df00d3dfe0dc0fac7d61051e0f453d
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-07-08 17:17:19 +0200

    Update Player-Video-Il-silenzio-della-natura-desktop.js

--- Player-Video-Il-silenzio-della-natura-desktop.js
+++ Player-Video-Il-silenzio-della-natura-desktop.js
@@ -736,7 +736,7 @@ justify-content:flex-end;
 
    // Javascript (JS)
 // ——— Lightbox → apri player con animazione ———
-const lightbox = document.getElementById('Open-Player-Video-Il-silenzio-della-natura-container-desktops');
+const lightbox = document.getElementById('Open-Player-Video-Il-silenzio-della-natura-container-desktop');
 lightbox.addEventListener('click', e => {
   e.preventDefault();
 
