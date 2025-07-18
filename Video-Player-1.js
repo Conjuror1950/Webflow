33a496804ff5be4e7a637ca1052399d5e0090d6e
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-07-18 11:05:21 +0200

    Update Video-Player-1.js

--- Video-Player-1.js
+++ Video-Player-1.js
@@ -290,7 +290,7 @@ margin:0 .2rem; }
   order: 1;           /* ← posa la serie dopo l’episodio */
   flex-basis: auto;   /* ← occupa tutta la riga disponibile */
   margin-right: auto; /* spinge i controlli verso destra */
-  margin-top: -4px;
+  margin-top: -2px;
   font-weight:500;
   font-size:14px;
   color: white;
