0b9ce7279e0d510e2ababd7d5de0da2d967b1989
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-07-19 14:50:32 +0200

    Update Video-Player-1.js

--- Video-Player-1.js
+++ Video-Player-1.js
@@ -690,7 +690,7 @@ justify-content:flex-end;
 
  @media (orientation: portrait) {
    .center-controls-player-video-il-silenzio-della-natura-mobile {
-     gap: 2.2rem !important;  /* se vuoi usare px: gap: 32px !important; */
+     gap: 2.4rem !important;  /* se vuoi usare px: gap: 32px !important; */
    }
 }
 
