f5833c40dcd8799dd8d196ed69ca2ee8c675bf7b
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-07-23 00:01:44 +0200

    Update Video-Player-1.js

--- Video-Player-1.js
+++ Video-Player-1.js
@@ -3,16 +3,19 @@
   // 1) INIETTA IL CSS
   const css = `
 
-  /* quando il dispositivo è in landscape, ruota indietro tutto il body */
-  @media only screen and (orientation: landscape) {
-    body {
-      transform: rotate(-90deg);
-      transform-origin: top left;
-      width: 100vh;   /* forza l’altezza come se fosse la larghezza */
-      height: 100vw;  /* forza la larghezza come se fosse l’altezza */
-      overflow: hidden;
-    }
-  } 
+/* solo su schermi “stretti” (mobile) in landscape, ruota indietro tutto il body */
+@media only screen 
+  and (max-width: 767px) 
+  and (orientation: landscape) {
+  
+  body {
+    transform: rotate(-90deg);
+    transform-origin: top left;
+    width: 100vh;   /* forza l’altezza come se fosse la larghezza */
+    height: 100vw;  /* forza la larghezza come se fosse l’altezza */
+    overflow: hidden;
+  }
+}
   
   /* Nascondi tutti i controlli custom */
 .controls-player-video-il-silenzio-della-natura-mobile {
