928e54cc1cec647668bbea8276a5144249f5b393
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-08-10 13:14:35 +0200

    Update Video-Player-1.js

--- Video-Player-1.js
+++ Video-Player-1.js
@@ -12,6 +12,7 @@
   transform:  translateY(0) !important;
   background: black    !important;
   z-index:    9999     !important;
+  overflow: visible !important; /* aggiungi */
 }
 
 /* In più, per sicurezza su Android/Chrome, definisci regole anche per il <video> in fullscreen diretto */
@@ -20,16 +21,9 @@ video:-webkit-full-screen,
 video:-ms-fullscreen {
   width:  100% !important;
   height: 100% !important;
-  object-fit: contain !important;
+  object-fit: cover !important;
   background: black !important;
   z-index: 9999 !important;        /* FORZA il video sopra tutto */
-}
-
-/* costringi Chrome/Android a mostrare i suoi controlli */
-video::-webkit-media-controls,
-video::-webkit-media-controls-enclosure {
-  display: block !important;
-  opacity: 1       !important;
 }
 
 .apple-video-wrapper-player-video-il-silenzio-della-natura-mobile {
@@ -143,21 +137,18 @@ lightbox.addEventListener('click', e => {
 
 // 4) Entra in fullscreen
 if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-  // iOS Safari
   if (vid.webkitEnterFullscreen) {
     vid.webkitEnterFullscreen();
   }
 } else if (/Android/.test(navigator.userAgent)) {
-  // Android: fullscreen DIRETTO sul video, non sul wrapper
   if (vid.requestFullscreen) {
-    vid.requestFullscreen();
+    vid.requestFullscreen().catch(err => console.warn(err));
   } else if (vid.webkitRequestFullscreen) {
     vid.webkitRequestFullscreen();
   } else if (vid.msRequestFullscreen) {
     vid.msRequestFullscreen();
   }
 } else {
-  // altri: fullscreen sul wrapper
   if (wrapper.requestFullscreen) {
     wrapper.requestFullscreen();
   } else if (wrapper.webkitRequestFullscreen) {
