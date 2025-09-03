df792d3e7090443884cff79bb4b207c47ec63f2c
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-09-03 15:10:04 +0200

    Update Gallery-Volume3-interni-e-scenari-mobile.js

--- Gallery-Volume3-interni-e-scenari-mobile.js
+++ Gallery-Volume3-interni-e-scenari-mobile.js
@@ -329,8 +329,8 @@
   var galleryHTML = `
 <div class="wrapper-slider-Volume3-interni-e-scenari-mobile">
   <div class="slider-title-top-Volume3-interni-e-scenari-mobile">
-    <h2></h2> /* Eliminato il titolo 'Interni e Scenari' */
-    <p></p> /* Eliminato il titolo 'Volume 3' */
+    <h2></h2>
+    <p></p>
   </div>
   
   <!-- Container che contiene solo lo slider -->
