0eba2ea1840d24147a69722de2bba7f78268d69f
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-09-11 12:27:27 +0200

    Update Gallery-Volume2-paesaggi-naturali-desktop.js

--- Gallery-Volume2-paesaggi-naturali-desktop.js
+++ Gallery-Volume2-paesaggi-naturali-desktop.js
@@ -321,26 +321,6 @@
       font-size: 13px;
       color: #86868b;
     }
-    .slider-title-bottom-Volume2-paesaggi-naturali-desktop {
-      font-family: "SF Pro Display", sans-serif !important;
-      max-width: 970px;
-      text-align: left;     
-      color: black;
-      margin-top: 0px;
-      margin-bottom: 15px;
-    }
-    .slider-title-bottom-Volume2-paesaggi-naturali-desktop h2 {
-      font-family: "SF Pro Display", sans-serif !important;
-      font-weight: 600;
-      font-size: 14px;
-      margin-bottom: -2px;
-    }
-    .slider-title-bottom-Volume2-paesaggi-naturali-desktop p {
-      font-family: "SF Pro Display", sans-serif !important;
-      font-weight: 400;
-      font-size: 13px;
-      color: #86868b;
-    }
 
     /* Media query: visualizza solo su desktop (>= 1280px) */
     @media screen and (max-width: 1279px) {
@@ -417,14 +397,6 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="slider-title-bottom-Volume2-paesaggi-naturali-desktop">
-        <h2>Specifiche</h2>
-        <p>Realizzato con Canon EOS 2000D e obiettivo 18-55mm EF-S. Il formato RAW (.CR2) garantisce una libertà totale in post-produzione, gestita attraverso Adobe Lightroom Classic. La raccolta contiene 10 elementi, immagini in HD (High Definition) scattate in Italia, negli anni 2024-2022.</p>
-        <h2>Dettagli</h2>
-        <p>· Scatti 1-2: Pisa, Toscana, Italia · 31 ott 2024</p>
-        <p>· Scatti 3-8: Lucca, Toscana, Italia · 19 ago 2024</p>
-        <p>· Scatti 9-10: Palermo, Sicilia, Italia · 14 ago 2022</p>
       </div>
     </div>
   `;
