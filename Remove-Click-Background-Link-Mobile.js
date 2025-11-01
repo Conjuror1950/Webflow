17245ca9bd10a2634572d44805d7d2dabb2c4f1a
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-01 16:31:59 +0100

    Update Remove-Click-Background-Link-Mobile.js

--- Remove-Click-Background-Link-Mobile.js
+++ Remove-Click-Background-Link-Mobile.js
@@ -1,9 +1,14 @@
-// Remove-Click-Background-Link-Mobile.js
-const style = document.createElement('style');
-style.innerHTML = `
-  a, button {
-    -webkit-tap-highlight-color: transparent !important;
-    -webkit-touch-callout: none !important;
-  }
-`;
-document.head.appendChild(style);
+// Remove-Click-Background-Link-Touch.js
+
+// Controlla se il dispositivo ha input touch
+if (window.matchMedia("(pointer: coarse)").matches) {
+  const style = document.createElement('style');
+  style.innerHTML = `
+    a, button {
+      -webkit-tap-highlight-color: transparent !important;
+      -webkit-touch-callout: none !important;
+    }
+  `;
+  document.head.appendChild(style);
+}
+
