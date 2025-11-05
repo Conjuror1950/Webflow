b0a05a04341e9478d61bab67376cf869bb966e90
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-05 15:41:46 +0100

    Update Remove-Click-Background-Link-Mobile.js

--- Remove-Click-Background-Link-Mobile.js
+++ Remove-Click-Background-Link-Mobile.js
@@ -4,12 +4,8 @@
 if (window.matchMedia("(pointer: coarse)").matches) {
   const style = document.createElement('style');
   style.innerHTML = `
-    div[data-search="true"] a,
-    div[data-search="true"] button,
-    div[data-shop="true"] a,
-    div[data-shop="true"] button {
+    a, button {
       -webkit-tap-highlight-color: transparent !important;
-      -webkit-touch-callout: none !important;
     }
   `;
   document.head.appendChild(style);
