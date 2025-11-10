5067d918a300ca9b698c8bd2ab7e7f04c51dc8db
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-10 15:04:54 +0100

    Update Font-Fallback.js

--- Font-Fallback.js
+++ Font-Fallback.js
@@ -1,7 +1,7 @@
 const style = document.createElement("style");
 style.textContent = `
   :root {
-    --apple-font-stack: -apple-system, BlinkMacSystemFont, "SF Pro Text", "SF Pro Display", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    --apple-font-stack: -apple-system, BlinkMacSystemFont, "SF Pro Text", "SF Pro Display", "Helvetica Neue", Helvetica, Arial, sans-serif !important;
   }
   body, button, input, textarea, select {
     font-family: var(--apple-font-stack);
