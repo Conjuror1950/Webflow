c92caa729480a412a50f01207e4ff0a35fc454b0
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-21 22:00:37 +0100

    Update Apple-Support-Contact-Chat-Page-desktop.js

--- Apple-Support-Contact-Chat-Page-desktop.js
+++ Apple-Support-Contact-Chat-Page-desktop.js
@@ -46,6 +46,7 @@
     cursor: pointer;
     width: auto;          /* larghezza minima necessaria */
     align-self: flex-start; /* allinea il pulsante a sinistra invece di allargarsi */
+    margin-top: 20px; /* ← aumenta la distanza dagli input */
   }
 
   .apple-contact-error {
