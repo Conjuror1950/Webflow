d1db5a31f6fa4d619d8da26ed3757f9725f07c1d
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-22 16:41:46 +0100

    Update Apple-Support-Contact-Chat-Page-desktop.js

--- Apple-Support-Contact-Chat-Page-desktop.js
+++ Apple-Support-Contact-Chat-Page-desktop.js
@@ -35,6 +35,7 @@ const css = `
     transition: border 0.18s ease, box-shadow 0.18s ease, color 0.18s ease;
     background: transparent;
     color: inherit;
+    caret-color: default; /* <-- aggiungi qui */
   }
 
   .apple-contact-field input:focus {
@@ -47,6 +48,7 @@ const css = `
     border: 1px solid rgb(227,0,0);   /* bordo rosso*/
     box-shadow: none;
     color: rgb(227,0,0);              /* testo dentro l'input diventa rosso */
+    caret-color: default; /* <-- aggiungi qui */
     transition: border 0.18s ease, box-shadow 0.18s ease, color 0.18s ease;
   }
 
