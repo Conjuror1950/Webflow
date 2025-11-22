a38121ab52b937eda69a94293fdc72686343256e
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-22 12:15:23 +0100

    Update Apple-Support-Contact-Chat-Page-desktop.js

--- Apple-Support-Contact-Chat-Page-desktop.js
+++ Apple-Support-Contact-Chat-Page-desktop.js
@@ -87,7 +87,7 @@
 
 .apple-contact-field input:focus + label,
 .apple-contact-field input:not(:placeholder-shown) + label {
-  top: -4px;
+  top: 5px;
   left: 16px;
   font-size: 13px;
   padding: 0 4px;
