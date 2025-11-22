37e9d4190a47e37023174b96b9013dce3f7d1ae8
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-22 15:53:35 +0100

    Update Apple-Support-Contact-Chat-Page-desktop.js

--- Apple-Support-Contact-Chat-Page-desktop.js
+++ Apple-Support-Contact-Chat-Page-desktop.js
@@ -91,8 +91,8 @@
   }
 
   .field-error svg {
-    width: 16px;
-    height: 16px;
+    width: 14px;
+    height: 14px;
     flex: 0 0 16px;
   }
 
@@ -180,7 +180,7 @@ function buildForm(){
     if(!container) return;
     input.parentElement.classList.add('has-error');
     container.innerHTML = `
-<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="16" height="16">
+<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="16" height="16">
   <!-- cerchio rosso con interno trasparente -->
   <circle cx="12" cy="12" r="10" fill="transparent" stroke="rgb(227,0,0)" stroke-width="2"/>
   
