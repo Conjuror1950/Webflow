ce5fe9352779ef85eaf9caa04f5239f3c11c6d1d
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-11-25 17:51:39 +0100

    Update Apple-Support-Contact-Chat-Page-desktop.js

--- Apple-Support-Contact-Chat-Page-desktop.js
+++ Apple-Support-Contact-Chat-Page-desktop.js
@@ -558,7 +558,7 @@ wrap.innerHTML = `
       <!-- semplice icona check -->
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg">
         <circle cx="12" cy="12" r="11" stroke-width="1.2" fill="transparent"/>
-        <path d="M7.5 12.5l2.5 2.5L16.5 9.5" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M7.5 12.5l2.5 2.5L16.5 9.5" stroke="#064b23" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </div>
 
