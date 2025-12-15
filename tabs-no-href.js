46daeb5e22b3f5a649de9d03cad1d0615dd44e52
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-12-15 16:51:16 +0100

    Update tabs-no-href.js

--- tabs-no-href.js
+++ tabs-no-href.js
@@ -1,76 +1,56 @@
 (function () {
-  var FIXED_HREF = window.location.pathname + "#";
-  var currentIndex = 0;
+  const FIXED_HREF = window.location.pathname + "#";
+  let currentIndex = 0;
   const tabs = document.querySelectorAll(".w-tab-link");
+  const tabsMenu = tabs[0].parentElement;
 
-  function createUnderline(tab) {
-    let ul = tab.querySelector(".tab-underline");
-    if (!ul) {
-      ul = document.createElement("div");
-      ul.classList.add("tab-underline");
-      tab.appendChild(ul);
+  // Crea un singolo underline dinamico
+  let underline = document.createElement("div");
+  underline.classList.add("tab-underline");
+  tabsMenu.appendChild(underline);
 
-      // Stile CSS dinamico
-      ul.style.position = "absolute";
-      ul.style.bottom = "0px";
-      ul.style.left = "0px";
-      ul.style.height = "2px";
-      ul.style.width = "100%";
-      ul.style.backgroundColor = "black";
-      ul.style.transform = tab.classList.contains("current") ? "scaleX(1)" : "scaleX(0)";
-      ul.style.transformOrigin = "left center";
-      ul.style.transition = "transform 0.2s ease-out";
-      ul.style.pointerEvents = "none"; // per non interferire col click
-    }
-    return ul;
+  // Stile base
+  underline.style.position = "absolute";
+  underline.style.bottom = "0";
+  underline.style.height = "1px";
+  underline.style.backgroundColor = "black";
+  underline.style.transition = "left 0.2s ease-out, width 0.2s ease-out";
+
+  function positionUnderline(index) {
+    const tab = tabs[index];
+    const rect = tab.getBoundingClientRect();
+    const parentRect = tabsMenu.getBoundingClientRect();
+    const left = rect.left - parentRect.left;
+    const width = rect.width;
+
+    underline.style.left = left + "px";
+    underline.style.width = width + "px";
   }
 
-  function forceTabHref() {
+  // Posizione iniziale
+  window.addEventListener("load", function () {
+    positionUnderline(currentIndex);
+
     tabs.forEach((tab, i) => {
-      // Crea underline se non esiste
-      const underline = createUnderline(tab);
-
       // Forza href
-      if (tab.getAttribute("href") !== FIXED_HREF) {
-        tab.setAttribute("href", FIXED_HREF);
-      }
+      tab.setAttribute("href", FIXED_HREF);
 
-      if (!tab.dataset.fixedHref) {
-        tab.dataset.fixedHref = "true";
+      tab.addEventListener("click", function (e) {
+        e.preventDefault();
 
-        tab.addEventListener("click", function (e) {
-          e.preventDefault();
+        currentIndex = i;
+        positionUnderline(currentIndex);
 
-          tabs.forEach((t, j) => {
-            const ul = t.querySelector(".tab-underline");
-            if (!ul) return;
+        // Aggiorna classe current
+        tabs.forEach(t => t.classList.remove("current"));
+        tab.classList.add("current");
 
-            if (j === i) {
-              // direzione dinamica
-              ul.style.transformOrigin = i > currentIndex ? "left center" : "right center";
-              ul.style.transform = "scaleX(1)";
-            } else {
-              ul.style.transformOrigin = j > i ? "left center" : "right center";
-              ul.style.transform = "scaleX(0)";
-            }
-
-            t.classList.remove("current");
-          });
-
-          tab.classList.add("current");
-          currentIndex = i;
-
-          // Forza href dopo click (Webflow reinietta)
-          setTimeout(() => {
-            tab.setAttribute("href", FIXED_HREF);
-          }, 0);
-        });
-      }
+        // Ripristina href in caso Webflow lo reinietti
+        setTimeout(() => {
+          tab.setAttribute("href", FIXED_HREF);
+        }, 0);
+      });
     });
-  }
+  });
 
-  window.addEventListener("load", forceTabHref);
-
-  const observer = new MutationObserver(forceTabHref);
-  observer.observe(document.body, { subtree: true, attributes: true, attributeFilter: ["href", "class"] });
 })();
