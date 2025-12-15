328823692456dd7b28098111fc37c1541699f7f4
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-12-15 02:15:22 +0100

    Update tabs-no-href.js

--- tabs-no-href.js
+++ tabs-no-href.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const tabLinks = document.querySelectorAll(".w-tab-link");
 
+  tabLinks.forEach(function (tab) {
+    // Rimuove l'attributo href
+    tab.removeAttribute("href");
+
+    // Blocca qualsiasi navigazione
+    tab.addEventListener("click", function (e) {
+      e.preventDefault();
+    });
+  });
+});
