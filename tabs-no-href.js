dd93a1d53d3a6eca9b91b9d333e82077c9f18907
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2025-12-15 16:48:54 +0100

    Update tabs-no-href.js

--- tabs-no-href.js
+++ tabs-no-href.js
@@ -3,8 +3,33 @@
   var currentIndex = 0;
   const tabs = document.querySelectorAll(".w-tab-link");
 
+  function createUnderline(tab) {
+    let ul = tab.querySelector(".tab-underline");
+    if (!ul) {
+      ul = document.createElement("div");
+      ul.classList.add("tab-underline");
+      tab.appendChild(ul);
+
+      // Stile CSS dinamico
+      ul.style.position = "absolute";
+      ul.style.bottom = "0px";
+      ul.style.left = "0px";
+      ul.style.height = "2px";
+      ul.style.width = "100%";
+      ul.style.backgroundColor = "black";
+      ul.style.transform = tab.classList.contains("current") ? "scaleX(1)" : "scaleX(0)";
+      ul.style.transformOrigin = "left center";
+      ul.style.transition = "transform 0.2s ease-out";
+      ul.style.pointerEvents = "none"; // per non interferire col click
+    }
+    return ul;
+  }
+
   function forceTabHref() {
     tabs.forEach((tab, i) => {
+      // Crea underline se non esiste
+      const underline = createUnderline(tab);
+
       // Forza href
       if (tab.getAttribute("href") !== FIXED_HREF) {
         tab.setAttribute("href", FIXED_HREF);
@@ -18,16 +43,17 @@
 
           tabs.forEach((t, j) => {
             const ul = t.querySelector(".tab-underline");
-            if (ul) {
-              if (j === i) {
-                // Determina direzione
-                ul.style.transformOrigin = i > currentIndex ? "left center" : "right center";
-                ul.style.transform = "scaleX(1)";
-              } else {
-                ul.style.transformOrigin = j > i ? "left center" : "right center";
-                ul.style.transform = "scaleX(0)";
-              }
+            if (!ul) return;
+
+            if (j === i) {
+              // direzione dinamica
+              ul.style.transformOrigin = i > currentIndex ? "left center" : "right center";
+              ul.style.transform = "scaleX(1)";
+            } else {
+              ul.style.transformOrigin = j > i ? "left center" : "right center";
+              ul.style.transform = "scaleX(0)";
             }
+
             t.classList.remove("current");
           });
 
@@ -39,14 +65,6 @@
             tab.setAttribute("href", FIXED_HREF);
           }, 0);
         });
-      }
-
-      // Stato iniziale underline
-      const ul = tab.querySelector(".tab-underline");
-      if (ul) {
-        ul.style.transform = tab.classList.contains("current") ? "scaleX(1)" : "scaleX(0)";
-        ul.style.transformOrigin = "left center";
-        ul.style.transition = "transform 0.2s ease-out";
       }
     });
   }
