0544f7a4daff5feba10eea895374bf7ee3ea382c
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2026-01-04 15:40:24 +0100

    Update redirectSlug-cms.js

--- redirectSlug-cms.js
+++ redirectSlug-cms.js
@@ -1,24 +1,17 @@
-// redirect-single-cms-fixed.js
+// redirect-single-cms-webflow.js
 (function() {
-  'use strict';
-
   const TARGET_PATH = "/it-it/102555";
   const REDIRECT_URL = "/it-it/102555/manual";
 
   function safeRedirect() {
-    try {
-      if (window.location.pathname === TARGET_PATH) {
-        window.location.replace(REDIRECT_URL);
-      }
-    } catch(e) {
-      console.error('Redirect failed', e);
+    if (window.location.pathname === TARGET_PATH) {
+      window.location.replace(REDIRECT_URL);
     }
   }
 
   // redirect subito
   safeRedirect();
 
-  // redirect ripetuto ogni 1 secondo in caso di caricamenti AJAX
-  setInterval(safeRedirect, 1000);
-
+  // ripeti ogni 500ms in caso di caricamenti dinamici
+  setInterval(safeRedirect, 500);
 })();
