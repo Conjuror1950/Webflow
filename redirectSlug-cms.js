571137ce4657a34bc5d6eae80147517d56b33124
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2026-01-04 18:04:34 +0100

    Update redirectSlug-cms.js

--- redirectSlug-cms.js
+++ redirectSlug-cms.js
@@ -1,16 +1,10 @@
-// removeSlugVisualHoverSafe.js
+// removeSlugVisualHoverFinal.js
 // Mostra l'href senza lo slug finale solo in hover (in basso a sinistra)
-// L'href reale rimane sempre intatto per qualsiasi evento
+// Il click rimane puntato al link reale
 
 (function() {
   try {
-    const slugsToRemove = [
-      "/manual",
-      "/video",
-      "/album",
-      "/extra"
-    ];
-
+    const slugsToRemove = ["/manual", "/video", "/album", "/extra"];
     const links = document.querySelectorAll("a");
 
     links.forEach(link => {
@@ -25,30 +19,25 @@
         }
       });
 
-      // Salva l'URL pulito su data attribute
-      link.setAttribute("data-href-clean", displayHref);
-
-      // Solo per visualizzazione hover: crea un listener che mostra il clean href
+      // Solo hover: mostra href pulito
       link.addEventListener("mouseenter", () => {
-        // Hack: modifica temporaneamente l'href visibile solo in tooltip del browser
-        // Usiamo Object.defineProperty per creare un href "virtuale"
-        Object.defineProperty(link, 'href', {
-          get: () => displayHref,
-          configurable: true
-        });
+        link.setAttribute("data-original-href", realHref);
+        link.setAttribute("href", displayHref);
       });
 
+      // Quando il mouse lascia il link, ripristina href reale
       link.addEventListener("mouseleave", () => {
-        // Ripristina l'href reale
-        Object.defineProperty(link, 'href', {
-          get: () => realHref,
-          configurable: true
-        });
+        link.setAttribute("href", link.getAttribute("data-original-href"));
+      });
+
+      // Click: assicurati che il link reale venga sempre usato
+      link.addEventListener("click", () => {
+        link.setAttribute("href", realHref);
       });
     });
 
-    console.log("[removeSlugVisualHoverSafe.js] Attivo: href visivi modificati solo in hover");
+    console.log("[removeSlugVisualHoverFinal.js] Attivo: href visivi modificati solo in hover");
   } catch (e) {
-    console.error("[removeSlugVisualHoverSafe.js] Errore:", e);
+    console.error("[removeSlugVisualHoverFinal.js] Errore:", e);
   }
 })();
