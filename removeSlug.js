3d432b3a17c596c713380ba9dad96f6621f85db7
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2026-01-04 16:13:55 +0100

    Update removeSlug.js

--- removeSlug.js
+++ removeSlug.js
@@ -1,1 +1,21 @@
+// removeSlugEarly.js
+// Rimuove il secondo slug "/manual" il prima possibile
+// L'URL visivo viene aggiornato prima del rendering della pagina
 
+(function() {
+  try {
+    let currentPath = window.location.pathname;
+
+    if (currentPath.endsWith("/manual")) {
+      // Rimuovi "/manual"
+      let newPath = currentPath.replace(/\/manual$/, "");
+
+      // Aggiorna subito la barra degli indirizzi
+      history.replaceState({}, "", newPath);
+
+      console.log("[removeSlugEarly.js] URL visivo modificato:", newPath);
+    }
+  } catch (e) {
+    console.error("[removeSlugEarly.js] Errore:", e);
+  }
+})();
