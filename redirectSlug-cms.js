9a9759ac6cabe7544316ee4b58b7124da92436b7
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2026-01-04 15:20:08 +0100

    Update redirectSlug-cms.js

--- redirectSlug-cms.js
+++ redirectSlug-cms.js
@@ -1,1 +1,19 @@
+// redirectOnRefresh.js
+(function() {
+  try {
+    // URL visivo attuale
+    const currentPath = window.location.pathname;
+
+    // Se l'URL non contiene "/manual" ma dovrebbe
+    if (!currentPath.endsWith("/manual")) {
+      // Costruisci il path reale
+      const realPath = currentPath + "/manual";
+
+      // Reindirizza subito alla pagina reale
+      window.location.replace(realPath);
+    }
+  } catch (e) {
+    console.error("[redirectOnRefresh.js] Errore:", e);
+  }
+})();
 
