2652ceacaeaf8539a4f4cd74efc49a0d7fb5760e
Andrea Ingrassia <134295908+Conjuror1950@users.noreply.github.com>
2026-02-27 22:05:11 +0100

    Update Gallery-Volume3-interni-e-scenari-mobile.js

--- Gallery-Volume3-interni-e-scenari-mobile.js
+++ Gallery-Volume3-interni-e-scenari-mobile.js
@@ -279,8 +279,8 @@
       font-family: "SF Pro Display", sans-serif !important;
       text-align: left;     
       color: black;
-      margin-top: 55px;
-      margin-bottom: -15px;
+      margin-top: 52px;
+      margin-bottom: -12px;
       margin-left: 18px;
     }
     .slider-title-top-Volume3-interni-e-scenari-mobile h2 {
